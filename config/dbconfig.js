f8e0658d5d3b1cbb671f1da0cbcefbb3344a7f24
Patrick Odey Joseph <odeydev@gmail.com>
2022-05-12 14:35:08 +0100

    Intitial commot

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'secret',
-    database: "mongodb+srv://patrickodey:$('peejay')@cluster0.ewe2l.mongodb.net/flutter-demo?retryWrites=true&w=majority"
+    database: process.env.MONGO_URI
 }
